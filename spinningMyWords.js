057cf842750db7bdf0bda730f141137b26cf7b96
Jahidul Sany <68532219+JahidulSany@users.noreply.github.com>
2023-11-30 12:17:05 +0000

    Spinning My Words Problem Solved

---
+++ spinningMyWords.js
@@ -0,0 +1,12 @@
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed. Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+const spinningWords = (string) => {
+  let words = string.split(' ').map(str => {
+    if(str.length >= 5) {
+      str = str.split('').reverse().join('');
+    }
+  return str;
+}).join(' ');
+  console.log(words);
+}
+spinningWords('React is a javaScript library');
